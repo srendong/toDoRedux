1e562a6fc5bccad7cc4b7ffa11e39e50910d91e6
Samuel Rendon G <s.redon.g@gmail.com>
2019-09-24 14:40:30 -0500

    Updates

---
+++ precache-manifest.5f8815ce27f9a8a9001373490b5eec76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8217e29d4b32b439a849c9e7d25e4efd",
+    "url": "/toDoRedux/index.html"
+  },
+  {
+    "revision": "76f30eff80b6ceb22cfb",
+    "url": "/toDoRedux/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "36b95eca6531d09b4d04",
+    "url": "/toDoRedux/static/css/main.be68a827.chunk.css"
+  },
+  {
+    "revision": "76f30eff80b6ceb22cfb",
+    "url": "/toDoRedux/static/js/2.b3aa490a.chunk.js"
+  },
+  {
+    "revision": "36b95eca6531d09b4d04",
+    "url": "/toDoRedux/static/js/main.53d5a199.chunk.js"
+  },
+  {
+    "revision": "6a608a793d76cbbe3bfe",
+    "url": "/toDoRedux/static/js/runtime~main.40fa53e3.js"
+  }
+]);
